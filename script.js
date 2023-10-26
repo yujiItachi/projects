97541c3694ac2fa93c3f7afdaf2c40eaca0c1564
Bhuvan Sharma <bhsharma022@gmail.com>
2023-10-26 11:50:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// gsap.to("h1", {
+//   x: 100,
+//   color: "red",
+//   duration: 2,
+//   y: 200,
+// });
+
+gsap.from("h1", {
+  autoAlpha: 0, // in css the property of the h1 is visibility:hidde;
+  duration: 4,
+});
